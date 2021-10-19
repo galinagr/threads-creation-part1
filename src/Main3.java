ac1f8a8a128406618c48bcacdc1fabb7325aaf59
ggryazno <ggryazno@cisco.com>
2021-10-19 18:45:05 +0300

    - some examples of threads running

---
+++ src/Main3.java
@@ -0,0 +1,15 @@
+public class Main3 {
+
+
+    public static void main(String[] args) {
+        Thread thread = new NewThread();
+        thread.start();
+    }
+
+    private static class NewThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from " + this.getName());
+        }
+    }
+}
